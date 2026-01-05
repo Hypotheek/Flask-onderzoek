7b06d24d83bc5615f65d11529ff1476c80b898ad
Ben <ben.bosman@student.hu.nl>
2026-01-06 00:18:44 +0100

    Blog works via postgres now

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -8,12 +8,42 @@ from flaskr.db import get_db
 
 bp = Blueprint('blog', __name__)
 
+
 @bp.route('/')
 def index():
     db = get_db()
-    posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
-    ).fetchall()
+    with db.cursor() as cur:
+        cur.execute(
+            'SELECT p.id, title, body, created, author_id, username '
+            'FROM post p JOIN "user" u ON p.author_id = u.id '
+            'ORDER BY created DESC'
+        )
+        posts = cur.fetchall()  # returns list of dicts because of dict_row
     return render_template('blog/index.html', posts=posts)
+
+
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            with db.cursor() as cur:
+                cur.execute(
+                    'INSERT INTO post (title, body, author_id) '
+                    'VALUES (%s, %s, %s)',
+                    (title, body, g.user['id'])
+                )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/create.html')
