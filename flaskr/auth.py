b0e50398c62d36b24aa78adc5e13ae0b4b03fede
Ben <ben.bosman@student.hu.nl>
2026-01-06 00:12:17 +0100

    auth.py works via postgres now, html templates in the right folder

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,13 +1,14 @@
 import functools
-
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from werkzeug.security import check_password_hash, generate_password_hash
+from psycopg.errors import UniqueViolation
 
 from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
@@ -24,12 +25,14 @@ def register():
 
         if error is None:
             try:
-                db.execute(
-                    "INSERT INTO user (username, password) VALUES (?, ?)",
-                    (username, generate_password_hash(password)),
-                )
+                with db.cursor() as cur:
+                    cur.execute(
+                        "INSERT INTO \"user\" (username, password) VALUES (%s, %s)",
+                        (username, generate_password_hash(password))
+                    )
                 db.commit()
-            except db.IntegrityError:
+            except UniqueViolation:
+                db.rollback()  # rollback to clear failed transaction
                 error = f"User {username} is already registered."
             else:
                 return redirect(url_for("auth.login"))
@@ -38,6 +41,7 @@ def register():
 
     return render_template('auth/register.html')
 
+
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
@@ -45,9 +49,10 @@ def login():
         password = request.form['password']
         db = get_db()
         error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
+
+        with db.cursor() as cur:
+            cur.execute('SELECT * FROM "user" WHERE username = %s', (username,))
+            user = cur.fetchone()
 
         if user is None:
             error = 'Incorrect username.'
@@ -63,6 +68,7 @@ def login():
 
     return render_template('auth/login.html')
 
+
 @bp.before_app_request
 def load_logged_in_user():
     user_id = session.get('user_id')
@@ -70,21 +76,22 @@ def load_logged_in_user():
     if user_id is None:
         g.user = None
     else:
-        g.user = get_db().execute(
-            'SELECT * FROM user WHERE id = ?', (user_id,)
-        ).fetchone()
+        db = get_db()
+        with db.cursor() as cur:
+            cur.execute('SELECT * FROM "user" WHERE id = %s', (user_id,))
+            g.user = cur.fetchone()
+
 
 @bp.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('index'))
 
+
 def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         if g.user is None:
             return redirect(url_for('auth.login'))
-
         return view(**kwargs)
-
     return wrapped_view
